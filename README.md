32b6cfb148b2fe72653d013eb2c7fbeb9e1bac8e
anurag <anuraggupta3979@gmail.com>
2021-12-12 10:10:44 +0530

    start

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1 align="center">Welcome to Anurag Gupta Portfolio 👋</h1>
---
+++ portfolio.js
@@ -0,0 +1,208 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "Anurag Gupta",
+  title: "Hi all, I'm Anurag",
+  description:
+    "A passionate Full Stack Web Developer and React Native Mobile App Developer having an experience of building Web applications with HTML / CSS / JavaScript / Reactjs / Nextjs / Nodejs / Bootstrap / Material UI / Ant Design and some other cool libraries and frameworks and Databases (MongoDB, SQL, Firebase..)",
+  resumeLink: "#",
+};
+
+export const openSource = {
+  githubUserName: "Anuraggupta3979",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  facebook: "https://www.facebook.com/anuraggupta.anuraggupta.395/",
+  instagram: "https://www.instagram.com/anurag_gupta3979/",
+  twitter: "https://twitter.com/anuraggupta3979",
+  github: "https://github.com/Anuraggupta3979",
+  linkedin: "https://www.linkedin.com/in/anurag-gupta-9a76b7201/",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    ),
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    // {
+    //   skillName: "flutter",
+    //   fontAwesomeClassname: "logos:flutter",
+    // },
+    // {
+    //   skillName: "swift",
+    //   fontAwesomeClassname: "vscode-icons:file-type-swift",
+    // },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "vscode-icons:file-type-sql",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "logos:aws",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "logos:firebase",
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "logos:python",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+    // {
+    //   skillName: "docker",
+    //   fontAwesomeClassname: "logos:docker-icon",
+    // },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "Frontend/Design", //Insert stack or technology you have experience in
+    progressPercentage: "90", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "70",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "70",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "Ashok Mahan School, Lalsot",
+    subHeader: "10th",
+    duration: "July 2015 - April 2016",
+    desc: "Board of Secondary Education, Rajasthan",
+    descBullets: ["81%"],
+  },
+  {
+    schoolName: "Ashok Mahan School, Lalsot",
+    subHeader: "12th",
+    duration: "July 2017 - April 2018",
+    desc: "Board of Secondary Education, Rajasthan",
+    descBullets: ["85%"],
+  },
+
+  {
+    schoolName: "MBM University, Jodhpur",
+    subHeader: "B.E in Information Technology",
+    duration: "August 2019 ",
+    // desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+    descBullets: ["CGPA - 9.4"],
+  },
+];
+
+export const experience = [
+  {
+    role: "Software Engineer",
+    company: "Google",
+    companylogo: "/img/icons/common/google.svg",
+    date: "June 2018 – Present",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    descBullets: [
+      "Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ],
+  },
+  {
+    role: "Front-End Developer",
+    company: "Github",
+    companylogo: "/img/icons/common/github.svg",
+    date: "May 2017 – May 2018",
+    desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    role: "Software Engineer Intern",
+    company: "Airbnb",
+    companylogo: "/img/icons/common/airbnbLogo.png",
+    date: "Jan 2015 – Sep 2015",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+export const projects = [
+  {
+    name: "lorem ipsum",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    link: "https://example.com",
+  },
+  {
+    name: "lorem ipsum",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://github.com/1hanzla100",
+  },
+  {
+    name: "lorem ipsum",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://github.com/1hanzla100",
+    link: "https://example.com",
+  },
+];
+
+export const feedbacks = [
+  {
+    name: "Hassan Mehmood",
+    feedback:
+      "We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
+  },
+  {
+    name: "Maheen Altaf",
+    feedback:
+      "the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+  },
+];
