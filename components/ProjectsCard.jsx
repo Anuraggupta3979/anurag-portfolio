667a8327cc431f3ca03af534b8288ffcc7805615
anurag <anuraggupta3979@gmail.com>
2021-12-13 14:17:53 +0530

    buttonlink

--- components/ProjectsCard.jsx
+++ components/ProjectsCard.jsx
@@ -17,7 +17,7 @@ const ProjectsCard = ({ data }) => {
                   <Button
                     className="btn-icon"
                     color="github"
-                    href={data.link}
+                    href={data.github}
                     target="_blank"
                     rel="noopener"
                     aria-label="Github"
