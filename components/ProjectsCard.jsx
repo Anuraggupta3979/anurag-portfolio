fdf71d1b2865cda2dea317e574734e0d8bc2a131
anurag <anuraggupta3979@gmail.com>
2021-12-13 13:37:43 +0530

    image2

--- components/ProjectsCard.jsx
+++ components/ProjectsCard.jsx
@@ -12,37 +12,7 @@ const ProjectsCard = ({ data }) => {
           <CardBody>
             <div className="d-flex px-3">
               <div className="">
-                <h3>{data.name}</h3>
-
-                {data.github ? (
-                  <Button
-                    className="btn-icon"
-                    color="github"
-                    href={data.link}
-                    target="_blank"
-                    rel="noopener"
-                    aria-label="Github"
-                  >
-                    <span className="btn-inner--icon">
-                      <i className="fa fa-github" />
-                    </span>
-                  </Button>
-                ) : null}
-                {data.link ? (
-                  <Button
-                    className="btn-icon"
-                    color="success"
-                    href={data.link}
-                    target="_blank"
-                    rel="noopener"
-                    aria-label="Twitter"
-                  >
-                    <span className="btn-inner--icon">
-                      <i className="fa fa-arrow-right mr-2" />
-                    </span>
-                    <span className="nav-link-inner--text ml-1">Demo</span>
-                  </Button>
-                ) : null}
+                <h1>Anurag</h1>
               </div>
             </div>
           </CardBody>
