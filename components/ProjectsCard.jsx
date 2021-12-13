9ddec2097875ed2c39a30e05eff2a900110a879b
anurag <anuraggupta3979@gmail.com>
2021-12-13 14:49:10 +0530

    buttonlink222222222

--- components/ProjectsCard.jsx
+++ components/ProjectsCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardBody, Col,Button } from "reactstrap";
+import { Card, CardBody, Col, Button } from "reactstrap";
 import { Fade } from "react-reveal";
 
 const ProjectsCard = ({ data }) => {
@@ -12,38 +12,42 @@ const ProjectsCard = ({ data }) => {
             <div className="d-flex px-3">
               <div className="">
                 <h3>{data.name}</h3>
-
                 <img
                   src={data.vector}
                   alt="ProjectImage"
                   className="card-img"
                 />
-                <Button
-                  className="btn-icon"
-                  color="github"
-                  href={data.github}
-                  target="_blank"
-                  rel="noopener"
-                  aria-label="Github"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-github" />
-                  </span>
-                </Button>
+                <p className="description mt-3">{data.description}</p>
 
-                <Button
-                  className="btn-icon"
-                  color="success"
-                  href={data.link}
-                  target="_blank"
-                  rel="noopener"
-                  aria-label="Twitter"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-arrow-right mr-2" />
-                  </span>
-                  <span className="nav-link-inner--text ml-1">Demo</span>
-                </Button>
+                {data.github ? (
+                  <Button
+                    className="btn-icon"
+                    color="github"
+                    href={data.link}
+                    target="_blank"
+                    rel="noopener"
+                    aria-label="Github"
+                  >
+                    <span className="btn-inner--icon">
+                      <i className="fa fa-github" />
+                    </span>
+                  </Button>
+                ) : null}
+                {data.link ? (
+                  <Button
+                    className="btn-icon"
+                    color="success"
+                    href={data.link}
+                    target="_blank"
+                    rel="noopener"
+                    aria-label="Twitter"
+                  >
+                    <span className="btn-inner--icon">
+                      <i className="fa fa-arrow-right mr-2" />
+                    </span>
+                    <span className="nav-link-inner--text ml-1">Demo</span>
+                  </Button>
+                ) : null}
               </div>
             </div>
           </CardBody>
---
+++ projects.js
@@ -0,0 +1,165 @@
+export const projects = [
+  {
+    name: "MBM University Website",
+    description:
+      "ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml/",
+    vector: "/img/icons/common/mbm.png",
+  },
+  {
+    name: "PortFolio",
+    description:
+      "Lorem  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml/",
+    vector: "/img/icons/common/portfolio.png",
+  },
+  {
+    name: "The CraftJet",
+    description:
+      "Lorem ipsum  sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://thecraftjet.ml",
+    vector: "/img/icons/common/craftjet.png",
+  },
+  {
+    name: "Covid-19 Tracker",
+    description:
+      "Lorem ipsum dolor  amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/covid.png",
+  },
+  {
+    name: "Google Clone",
+    description:
+      "Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/google.png",
+  },
+  {
+    name: "Discord Clone",
+    description:
+      "Lorem ipsum dolor sit amet, coetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/discord.png",
+  },
+  {
+    name: "Netflix Clone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur acing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/netflix.png",
+  },
+  {
+    name: "Netflix Clone-2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/netflix2.png",
+  },
+  {
+    name: "Facebook Clone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit,  do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/portfolio.png",
+  },
+  {
+    name: "Spotify Clone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do  tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/spotify.png",
+  },
+  {
+    name: "ZOOM Clone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/portfolio.png",
+  },
+
+  {
+    name: "Instagram Reels Clone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna iqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/portfolio.png",
+  },
+
+  {
+    name: "Food Order App",
+    description:
+      "Lem ipm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/portfolio.png",
+  },
+
+  {
+    name: "Foodmeel",
+    description:
+      "L dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/portfolio.png",
+  },
+  {
+    name: "Chat app",
+    description:
+      "orem  dolor  amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/portfolio.png",
+  },
+  {
+    name: "Guess Number Game",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur  elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/guess.png",
+  },
+  {
+    name: "Mapbox",
+    description:
+      "Lorem ipsum dolor sit amet,  adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/portfolio.png",
+  },
+  {
+    name: "Pig Game",
+    description:
+      "Lorem ipsum  sit amet, cctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/piggame.png",
+  },
+  {
+    name: "Weather App",
+    description:
+      "Lorem ipsum dolor sit , consectetur adipiscing , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/weather.png",
+  },
+  {
+    name: "Love Calculator",
+    description:
+      "Loem ipsm door st amet, consctetur aipising eit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    github: "https://anurag-gupta.ml",
+    link: "https://anurag-gupta.ml",
+    vector: "/img/icons/common/love.png",
+  },
+];
